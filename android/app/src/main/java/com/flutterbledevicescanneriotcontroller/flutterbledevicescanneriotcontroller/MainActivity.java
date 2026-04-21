4e69c067d0c26252d00408a081a718cb04026a30
MohammedGhallab <mohammedfahmiyghallab@gmail.com>
2026-04-21 17:26:15 +0300

    Add initial iOS project setup with launch images, app icons, and main Flutter app structure
    - Created app icons in various resolutions for iOS.
    - Added launch images for different device scales.
    - Set up LaunchScreen storyboard and Main storyboard for the app.
    - Configured Info.plist with necessary app metadata.
    - Implemented basic Flutter app structure with a counter example.
    - Added unit tests for the Flutter app.
    - Included README for launch screen assets customization.

---
+++ android/app/src/main/java/com/flutterbledevicescanneriotcontroller/flutterbledevicescanneriotcontroller/MainActivity.java
@@ -0,0 +1,6 @@
+package com.flutterbledevicescanneriotcontroller.flutterbledevicescanneriotcontroller;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
